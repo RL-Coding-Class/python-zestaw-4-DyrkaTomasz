e6e7eafd9d3ee903c5eee4228c60752cd8eac15b
Tomasz Dyrka <tomasz.dyrka@student.uj.edu.pl>
2025-01-25 17:03:30 +0100

    test with different import

--- zadanie_1/main.py
+++ zadanie_1/main.py
@@ -1,7 +1,7 @@
 import schedule
 import time
-from zadanie_1.database import create_table
-from zadanie_1.flight_data import fetch_flight_data, plot_flight_data
+from ZADANIE1.database import create_table
+from ZADANIE1.flight_data import fetch_flight_data, plot_flight_data
 
 def main(interval, max_repeats):
     create_table(max_repeats)
